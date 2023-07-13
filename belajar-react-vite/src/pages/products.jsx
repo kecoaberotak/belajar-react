f18da1e31c6933fd9b2056eb80f1edc461d37ea8
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-13 20:13:06 +0700

    coba rendering list

--- belajar-react-vite/src/pages/products.jsx
+++ belajar-react-vite/src/pages/products.jsx
@@ -1,23 +1,34 @@
 import CardProduct from "../components/fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "Sepatu",
+    price: "Rp 1.000.000",
+    image: "images/luis-felipe-lins-LG88A2XgIXY-unsplash.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium eaque facilis dolor ipsum harum optio."
+  },
+  {
+    id: 2,
+    name: "Sepatu Dua",
+    price: "Rp 2.000.000",
+    image: "images/luis-felipe-lins-LG88A2XgIXY-unsplash.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+  },
+]
+
 const ProductsPage = () => {
   return(
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="images/luis-felipe-lins-LG88A2XgIXY-unsplash.jpg"/>
-        <CardProduct.Body title='Sepatu'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium eaque facilis dolor ipsum harum optio?
-        </CardProduct.Body>
-        <CardProduct.Footer price='Rp 1.000.000'/>
-      </CardProduct>
-
-      <CardProduct>
-        <CardProduct.Header image="images/luis-felipe-lins-LG88A2XgIXY-unsplash.jpg"/>
-        <CardProduct.Body title='Shoes'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, minima?
-        </CardProduct.Body>
-        <CardProduct.Footer price='Rp 5.000.000'/>
-      </CardProduct>
+      {products.map(product => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.image}/>
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price}/>
+        </CardProduct>
+      ))}
     </div>
   )
 }
