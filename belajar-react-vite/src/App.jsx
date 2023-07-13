c39c9254e99a409f4ef036462ef5168ab124206b
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-13 14:05:33 +0700

    download dan setting react router

--- belajar-react-vite/src/App.jsx
+++ belajar-react-vite/src/App.jsx
@@ -1,12 +1,8 @@
-// import LoginPage from "./pages/login"
-import RegisterPage from "./pages/register"
-
 function App() {
   return (
     <>
     <div className='flex justify-center min-h-screen items-center'>
-      {/* <LoginPage/> */}
-      <RegisterPage/>
+      <div>Hello World!</div>
     </div>
     </>
   )
--- belajar-react-vite/src/main.jsx
+++ belajar-react-vite/src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+// import pages
+import App from './App'
+import LoginPage from './pages/login'
+import RegisterPage from './pages/register'
+
+// import dan setting react router
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
