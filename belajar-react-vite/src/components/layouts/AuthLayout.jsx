ef7a1c6e77d8a168d855beb181a3723438b9ddb8
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-13 14:53:03 +0700

    conditional rendering buat page login dan register

--- belajar-react-vite/src/components/layouts/AuthLayout.jsx
+++ belajar-react-vite/src/components/layouts/AuthLayout.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
+
 const AuthLayout = (props) => {
-  const {children, title} = props;
+  const {children, title, type} = props;
   return (
     <div className='flex justify-center min-h-screen items-center'>
       <div className="w-full max-w-xs">
@@ -8,9 +10,17 @@ const AuthLayout = (props) => {
           Welcome, Please enter your details
         </p>
         {children}
+        {conditionalNav({type})}
       </div>
     </div>
   )
 };
+
+const conditionalNav = ({type}) => (
+  <p className="text-sm mt-5 text-center">
+    {type === 'login' ? 'Dont have an account? ' : 'Already have an account? '}
+    {type === 'login' ? <Link to="/register" className="font-bold text-blue-600">Sign up</Link> : <Link to="/login" className="font-bold text-blue-600">Login</Link>}
+  </p>
+)
 
 export default AuthLayout;
